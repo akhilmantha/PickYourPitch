af40b0926291f8f6748419e3264ac6be179e294a
akhilmantha <akhilmantha@live.com>
2018-07-13 11:45:08 +0530

    basic functionality of pick your pitch

---
+++ PickYourPitch/RecordedAudio.swift
@@ -0,0 +1,20 @@
+//
+//  RecordedAudio.swift
+//  Pick Your Pitch
+//
+//  Created by AkhilMantha on 14/5/18.
+//  Copyright (c) 2018 AkhilMantha. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - RecordedAudio
+
+struct RecordedAudio {
+    
+    // MARK: Properties
+    
+    var filePathUrl: URL!
+    var title: String!
+    
+}
